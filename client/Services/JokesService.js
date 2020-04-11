fb6ecfa0482b68804c826185d3e4fced8b5a351a
bayelimcadams <mcadamsbay@gmail.com>
2020-04-10 19:57:03 -0600

    .

--- client/Services/JokesService.js
+++ client/Services/JokesService.js
@@ -1,11 +1,21 @@
 import store from "../store.js";
+import Joke from "../Models/Joke.js"
 
 let _jokeApi = axios.create({
-  baseURL: "localhost:",
+  baseURL: "localhost:3000/jokes",
   timeout: 3000,
 });
 class JokesService {
-  getAllJokes() {}
+  async getAllJokes() {
+    try {let jokes = await _jokeApi.get()
+      console.log(jokes)
+      jokes.map(jokeRawData => new Joke(jokeRawData))
+      store.commit('jokes', jokes)
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
 }
 
 const jokesService = new JokesService();
