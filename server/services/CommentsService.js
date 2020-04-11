c742f5d5719cdd53c84f1e00ddbe4c1b4689f864
light-attracts-bugs <micahurizar@gmail.com>
2020-04-10 18:32:12 -0600

    fixed naming issue again

--- server/services/CommentsService.js
+++ server/services/CommentsService.js
@@ -3,23 +3,23 @@ import { BadRequest } from "../utils/Errors";
 
 class CommentsService {
   async edit(commentId, body) {
-    let comment = await dbContext.Comments.findByIdAndUpdate(commentId, body, { new: true })
+    let comment = await dbContext.Comment.findByIdAndUpdate(commentId, body, { new: true })
     return comment
   }
   async remove(commentId) {
-    let comment = await dbContext.Comments.findByIdAndDelete(commentId)
+    let comment = await dbContext.Comment.findByIdAndDelete(commentId)
     return comment
   }
   async create(body) {
-    let comment = await dbContext.Comments.create(body)
+    let comment = await dbContext.Comment.create(body)
     return comment
   }
   async getAll(query = {}) {
-    let comments = await dbContext.Comments.find(query).populate("tags");
+    let comments = await dbContext.Comment.find(query).populate("tags");
     return comments;
   }
   async getById(id) {
-    let comment = await dbContext.Comments.findById(id);
+    let comment = await dbContext.Comment.findById(id);
     if (!comment) {
       throw new BadRequest("Invalid Id");
     }
