5012778767754217ea99a55659159199cccda290
light-attracts-bugs <micahurizar@gmail.com>
2020-04-10 18:26:24 -0600

    added CommentSchema to DbContext

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -2,6 +2,7 @@ import ValueSchema from "../models/Value";
 import JokeSchema from "../models/Joke";
 import VoteSchema from "../models/Vote";
 import UserSchema from "../models/User";
+import CommentSchema from "../models/Comment";
 import mongoose from "mongoose";
 
 class DbContext {
@@ -9,6 +10,7 @@ class DbContext {
   Jokes = mongoose.model("Joke", JokeSchema);
   Votes = mongoose.model("Vote", VoteSchema);
   User = mongoose.model("User", VoteSchema);
+  Comment = mongoose.model("Comment", CommentSchema);
 }
 
 export const dbContext = new DbContext();
